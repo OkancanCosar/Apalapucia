a3b02bf40832d75a6b6764d393b346681b2a7edf
Erdem Gençoğlu <gencogluerdem@gmail.com>
2016-12-20 21:30:39 +0300

    css görme sorunu giderildi

--- app/controllers/worker_work_controller.rb
+++ app/controllers/worker_work_controller.rb
@@ -2,8 +2,6 @@ class WorkerWorkController < ApplicationController
   before_action :authenticate_worker!
 
   def index
-    @yetenekler = current_worker.skills
-    @firm = Company.find(current_worker.company_id)
-    @ilanlar = Announcament.where(:worker_id => current_worker.id)
+
   end
 end
