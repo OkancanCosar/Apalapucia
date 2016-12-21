af6ef0a83a2c9b417be711972773bb69ce53989b
OkancanCosar <okan1cosar@gmail.com>
2016-12-21 20:25:24 +0300

    full bugfix

--- app/controllers/company_work_controller.rb
+++ app/controllers/company_work_controller.rb
@@ -2,5 +2,7 @@ class CompanyWorkController < ApplicationController
   before_action :authenticate_company!
 
   def index
+    @cls = Worker.where(:company_id => current_company.id )
+    @an = Announcament.where(:company_id => current_company.id)
   end
 end
